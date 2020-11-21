a933fa9a77551ddf45bc48507fa2c5913befe187
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-11-21 14:48:47 -0500

    Demo: added video

--- demo/presentation.js
+++ demo/presentation.js
@@ -6,7 +6,7 @@ const presentation = {
         "demo/2010-03-20-118.webp",
         "demo/2012-05-19.70.webp",
         "demo/2019-05-18.088.webp",
-        "demo/2020-09-17.video.30.webp",
+        "demo/2020-09-17.video.32.webp",
      ],
      hideHelpOnStart: true,
      rtl: false,

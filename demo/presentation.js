461ba7a9f3fbd7842646cec15f13a51758418e07
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-11-21 13:40:05 -0500

    Demo: system presentation development

--- demo/presentation.js
+++ demo/presentation.js
@@ -1,16 +1,18 @@
 ﻿"use strict";
 const presentation = {
     images: [ // relative to presentation.html
+        "demo/Introduction.svg",
         "demo/2007-06-23.panorama.webp",
         "demo/2010-03-20-118.webp",
         "demo/2012-05-19.70.webp",
         "demo/2019-05-18.088.webp",
         "demo/2020-09-17.video.30.webp",
      ],
+     hideHelpOnStart: true,
      rtl: false,
      title: "Presentation Demo",
      colors: {
-         background: "lightGray",
+         background: "white",
          text: {
              background: "azure",
              foreground: "black",

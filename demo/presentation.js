8f2c6ba621bea3989fe367bd96fdb17c323a9c8d
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-11-20 19:53:11 -0500

    Code: improved error handling, eliminated false-positive error description blaming use presentation script in case of presentation.html document.body.onload exception

--- demo/presentation.js
+++ demo/presentation.js
@@ -5,6 +5,7 @@ const presentation = {
         "demo/2010-03-20-118.webp",
         "demo/2012-05-19.70.webp",
         "demo/2019-05-18.088.webp",
+        "demo/2020-09-17.video.30.webp",
      ],
      rtl: false,
      title: "Presentation Demo",

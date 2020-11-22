377f129f8c80b47fa48e1197088455d550554663
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-11-21 19:34:58 -0500

    Demo: all frames are added

--- demo/presentation.js
+++ demo/presentation.js
@@ -2,10 +2,14 @@
 const presentation = {
     images: [ // relative to presentation.html
         "demo/introduction.svg",
+        "demo/insights.svg",
+        "demo/images.svg",
+        "demo/look-at-photo.svg",
         "demo/2007-06-23.panorama.webp",
         "demo/2010-03-20-118.webp",
         "demo/2012-05-19.70.webp",
         "demo/2019-05-18.088.webp",
+        "demo/look-at-video.svg",
         "demo/2020-09-17.video.32.webp",
      ],
      hideHelpOnStart: true,

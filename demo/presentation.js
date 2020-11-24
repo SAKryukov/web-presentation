dc7151f6ccea5b03a5e49c9a92532296848163d4
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-11-24 01:19:53 -0500

    Demo: added prompt for animation

--- demo/presentation.js
+++ demo/presentation.js
@@ -9,13 +9,13 @@ const presentation = {
         "demo/2010-03-20-118.webp",
         "demo/2012-05-19.70.webp",
         "demo/2019-05-18.088.webp",
-        "demo/look-at-video.svg",
-        video(""),
-        video("demo/CapeCode.20151108.143402.webm", {
-                                                         title: "Cape Code, Provincetown, Blackwater Pond",
-                                                         poster: "demo/2015-11-08-ProvinceTown-Blackwater-Pond-11.webp" 
-        }),
+        "demo/look-at-animation.svg",
         "demo/2020-09-17.video.32.webp",
+        image("demo/look-at-video.svg"),
+        video("demo/CapeCode.20151108.143402.webm", {
+            title: "Cape Code, Provincetown, Blackwater Pond",
+            poster: "demo/2015-11-08-ProvinceTown-Blackwater-Pond-11.webp" 
+        }),
      ],
      title: "Presentation Demo",
      hideHelpOnStart: false,

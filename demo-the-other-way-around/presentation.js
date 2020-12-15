06bc769ffc2f3dc3bfcf5811e15c151c7e0007ed
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-12-14 20:13:53 -0500

    The other way around: removed iterator object which was useless, fixed and improved parsing

--- demo-the-other-way-around/presentation.js
+++ demo-the-other-way-around/presentation.js
@@ -1,59 +1,59 @@
 "use strict";
 
-const imageType = { image:0, html:1, video: 2, };
-const presentationFrameIterator = selector => {
+const frameType = { image: 0, html: 1, video: 2, };
+const presentationFrameParser = selector => {
+    const autoStartClass = "autostart";
     const videoAutostart = -1;
     const fileNameSeparator = ":";
     const validOptionSeparator = ", ";
-    const imageType = { image:0, html:1, video: 2, };
-    const imageTypeNames = ["image", "html", "video"];
-    const autoStartClass = "autostart";
-    let permittedTypeNames = imageTypeNames.join(validOptionSeparator);
-    permittedTypeNames += `${validOptionSeparator}${imageTypeNames[2]} ${autoStartClass}`;
-    const wrongClassMessage = text => `Unknown image type: "${text}". Supported types: ${permittedTypeNames}.`;
+    let frameTypeNames = [];
+    for (let frameTypeName in frameType) frameTypeNames.push(frameTypeName);
+    let permittedTypeNames = frameTypeNames.join(validOptionSeparator);
+    permittedTypeNames += `${validOptionSeparator}${frameTypeNames[2]} ${autoStartClass}`;
+    const wrongClassMessage = text => `Unknown frame type: "${text}". Supported types: ${permittedTypeNames}.`;
     const invalidClassAttributeMessage = text => `Unknown class value: "${text}". Supported types: ${permittedTypeNames}.`;
-    const getImageType = element => {
+    const getFrameType = element => {
         const classAttributeValue = element.getAttribute("class");
         if (!classAttributeValue)
-            return imageType.image;
+            return frameType.image;
         const classes = element.classList;
         if (classes.length == 1) {
             let index = 0;
-            for (let name of imageTypeNames) {
+            for (let name of frameTypeNames) {
                 if (name.toLowerCase() == classes[0].toLowerCase())
                     return index;
                 ++index;
             } //loop
             return wrongClassMessage(classes[0]);
         } else if (classes.length == 2) {
-            if ((classes[0].toLowerCase() == imageTypeNames[imageType.video] && classes[1].toLowerCase() == autoStartClass)
-                || (classes[1].toLowerCase() == imageTypeNames[imageType.video] && classes[0].toLowerCase() == autoStartClass))
+            if ((classes[0].toLowerCase() == frameTypeNames[frameType.video] && classes[1].toLowerCase() == autoStartClass)
+                || (classes[1].toLowerCase() == frameTypeNames[frameType.video] && classes[0].toLowerCase() == autoStartClass))
                 return videoAutostart; // autostart video
             else
                 return invalidClassAttributeMessage(classAttributeValue);
         } else
             return invalidClassAttributeMessage(classAttributeValue);
-    } //getImageType
-    //
+    } //getFrameType
+    // creating and returning frames:
     const frames = [];
-    const images = document.querySelectorAll(selector);
-    for (let element of images) {
+    const frameElements = document.querySelectorAll(selector);
+    for (let element of frameElements) {
         if (element.innerHTML.trim().length < 1)
             return `Invalid empty text content of the element "${element.tagName.toLowerCase()}"`;
-        const typeInfo = getImageType(element);
+        const typeInfo = getFrameType(element);
         if (typeInfo.constructor == String) // error
             return typeInfo;
-        if (typeInfo == imageType.html) {
+        if (typeInfo == frameType.html) {
             frames.push({ type: typeInfo, html: element.innerHTML });
             continue;
         } //if html
         const files = element.textContent.split(fileNameSeparator);
         if (files.length < 1 || files[0].length < 1)
             return `Invalid list of file names in the text content "${element.textContent}" of the element "${element.tagName.toLowerCase()}": at least one file name should be provided`;
-        if (typeInfo == videoAutostart || typeInfo == imageType.video) { // video, autostart or not
+        if (typeInfo == videoAutostart || typeInfo == frameType.video) { // video, autostart or not
             if (files.length != 1 && files.length != 2)
                 return `Invalid list of file names in the text content "${element.textContent}" of the element "${element.tagName.toLowerCase()}": exactly one video file should be specified, an optional second file is the image file uses as a poster`;
-            const videoFrameInfo = { type: imageType.video, file: files[0].trim() };
+            const videoFrameInfo = { type: frameType.video, file: files[0].trim() };
             if (files.length == 2) videoFrameInfo.poster = files[1].trim();
             if (typeInfo == true)  videoFrameInfo.autostart = typeInfo == videoAutostart;
             frames.push(videoFrameInfo);
@@ -62,19 +62,17 @@ const presentationFrameIterator = selector => {
         for (let file of files)
             frames.push({ type: typeInfo, file: file.trim()});
     } //loop
-    let current = -1;
-    const next = function() {
-        ++current;
-        return { done: current > frames.length - 1, value: frames[current] };
-    }; //next
-    return { next: next, [Symbol.iterator]: function() { return this; } };
-}; //presentationFrameIterator
+    return frames;
+}; //presentationFrameParser
 
 const optionParser = selector => {
-    const validOptionNames = ["hideHelpOnStart", "background", "rtl"];
+    const optionDefaults = { hideHelpOnStart: false, rtl: false, background: "white" };
+    let validOptionNames = [];
+    for (let optionName in optionDefaults) validOptionNames.push(optionName);
     const messageValidOptionNames = validOptionNames.join(", ");
-    const options = {};
+    const options = optionDefaults;
     const select = document.querySelectorAll(selector);
+    if (select.length < 1) return options;
     if (select.length > 1) return `Only one "body > select" element should be specified; it is used for the specification of the presentation options`;
     for (let option of select[0]) {
         let valid = false;
@@ -91,14 +89,21 @@ const optionParser = selector => {
 
 window.onload = () => {
 
-    const iterator = presentationFrameIterator("body > *:not(select)");
+    const frames = presentationFrameParser("body > *:not(select)");
     const options = optionParser("body > select");
-    if (iterator.constructor == String)
+    document.body.innerHTML = "";
+    if (frames.constructor == String)
         return alert(iterator);
     if (options.constructor == String)
         return alert(options);
-    for (let frame of iterator) {
+    
+    const htmlContainer = document.createElement("main");
+    htmlContainer.display = "block";
+    document.body.appendChild(htmlContainer);
+    for (let frame of frames) {
         const frame1 = frame;
+        if (frame.type == frameType.html)
+            htmlContainer.innerHTML = frame.html;
     } //loop
 
     const savedStyle = {

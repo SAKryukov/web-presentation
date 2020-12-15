cf37171feb34ddf354be53a8358c1db15f8a30c9
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-12-15 14:15:36 -0500

    Demo, code: all: resourced moved to "demo-resources", to share between two kinds of demo

--- demo/presentation.js
+++ demo/presentation.js
@@ -1,20 +1,20 @@
 ﻿"use strict";
 const presentation = {
     images: [ // relative to presentation.html
-        image("demo/introduction.svg"),
-        "demo/insights.svg",
-        "demo/images.svg",
-        "demo/look-at-photo.svg",
-        "demo/2007-06-23.panorama.webp",
-        "demo/2010-03-20-118.webp",
-        "demo/2012-05-19.70.webp",
-        "demo/2019-05-18.088.webp",
-        "demo/look-at-animation.svg",
-        image("demo/2020-09-17.video.32.webp"),
-        image("demo/look-at-video.svg"),
-        video("demo/CapeCode.20151108.143402.webm", {
+        image("demo-resources/introduction.svg"),
+        "demo-resources/insights.svg",
+        "demo-resources/images.svg",
+        "demo-resources/look-at-photo.svg",
+        "demo-resources/2007-06-23.panorama.webp",
+        "demo-resources/2010-03-20-118.webp",
+        "demo-resources/2012-05-19.70.webp",
+        "demo-resources/2019-05-18.088.webp",
+        "demo-resources/look-at-animation.svg",
+        image("demo-resources/2020-09-17.video.32.webp"),
+        image("demo-resources/look-at-video.svg"),
+        video("demo-resources/CapeCode.20151108.143402.webm", {
             title: "Cape Code, Provincetown, Blackwater Pond",
-            poster: "demo/2015-11-08-ProvinceTown-Blackwater-Pond-11.webp",
+            poster: "demo-resources/2015-11-08-ProvinceTown-Blackwater-Pond-11.webp",
             start: false
         }),
      ],
